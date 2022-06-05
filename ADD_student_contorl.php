5b82d431bf4c4a7549bcb61a9735abd4b16991e1
SE7EN404 <100765912+SE7EN404@users.noreply.github.com>
2022-06-05 19:13:56 +0200

    ;

---
+++ ADD_student_contorl.php
@@ -0,0 +1,31 @@
+<?php 
+include_once("Student.php");
+include_once("Admin.php");
+
+$name=$_REQUEST["Name"];
+$password=$_REQUEST["Password"];
+$religion=$_REQUEST["Religion"];
+$class=$_REQUEST["ClassName"];
+$level=$_REQUEST["Level"];
+$Email=$_REQUEST["Email"];
+$obj=new Student;
+$array=[]; 
+$array=$obj->listAll();
+$flag=0;
+for($i=0;$i<count($array);$i++)
+{
+   if($array[$i]->Email==$Email)
+   {
+    $flag=1;
+   }
+}
+if($flag==0)
+{
+     $AdminX=new Admin1;
+     $AdminX->addST($name,$password,$Email,$level,$class,$religion);
+     header("location:readStudent.php");
+
+}
+
+
+?>
